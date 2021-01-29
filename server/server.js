7b2601a95fe9e33c3101ca06e550831d50b56c47
Mark Pelka <mark.pelka@gmail.com>
2021-01-29 12:46:01 -0500

    completed my api, set up backend routes

--- server/server.js
+++ server/server.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const stretch = require('./stretches.json')
 app.use(cors());
+
+app.get('/stretches', (req, res) => {
+  return res.send(stretch)
+})
+
+app.use(express.json());
+app.route('/stretches/:id').get ((req, res) => {
+let currentStretch = stretch.find((stretch) => stretch.id == req.params.id);
+
+if(currentStretch === undefined) {
+  return res
+  .status(404)
+  .send(`Stretch gif ID of ${req.params.id} does not exist`)
+};
+return res.send(currentStretch)
+});
 
 
 app.listen (8080, () => {
